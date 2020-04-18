b64acc8447d41f05bbd502277c8add303e2d7963
Henning Classen <henning@clssn.de>
2020-04-18 22:13:03 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="numato-gpio",
-    version="0.6.0",
+    version="0.7.0.dev0",
     author="Henning Classen",
     author_email="code@clssn.de",
     description="Python API for Numato GPIO Expanders",
