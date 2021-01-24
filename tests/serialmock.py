62217f8fd9d5b9bfac09831d1fe7b06a3ae3c8b7
Henning Claßen <code@clssn.de>
2021-01-24 23:05:38 +0100

    Inject notifications into responses in device mockup
    If notifications are enabled, the mockup now inserts notifications
    at configurable positions in the device response.

--- tests/serialmock.py
+++ tests/serialmock.py
@@ -8,6 +8,7 @@ def gen_serial(eol):
         def respond(self, input):
             responses = {
                 b'gpio notify off\r': b'gpio notify disabled\n>' if self.ports != 8 else b'',
+                b'gpio notify on\r': b'gpio notify enabled\n>' if self.ports != 8 else b'',
                 b'id get\r': b'00004711\n>',
                 b'ver\r': b'00000008\n>',
                 b'gpio readall\r': f'{"0"*(self.ports//4)}\n>'.encode(),
@@ -18,7 +19,16 @@ def gen_serial(eol):
                 f'gpio iodir {"f"*(self.ports//4)}\r'.encode(): b'>',
                 f'gpio iodir {"F"*(self.ports//4)}\r'.encode(): b'>',
             }
-            return responses[input].decode().replace("\n", self.eol).encode()
+            resp = input.decode().replace("\r", self.eol)
+            resp += responses[input].decode().replace("\n", self.eol)
+            if self.notify:
+                msg = "{eol}# {xff} {x00} {xff}".format(
+                    eol=self.eol,
+                    xff="F"*(self.ports//4),
+                    x00="0"*(self.ports//4),
+                )
+                resp = resp[:self.notify_inject_at] + msg + resp[self.notify_inject_at:]
+            return resp.encode()
 
         def __init__(self, file, speed, timeout):
             self.file = file
@@ -28,13 +38,23 @@ def gen_serial(eol):
             self.lock = threading.RLock()
             self.is_open = True
             self.eol = eol
+            self.notify = False
+            self.notify_inject_at = 0
 
         def write(self, input):
             """Write to the mocked device.
 
             Processes the written data and generates the output in the buffer."""
             with self.lock:
-                self.buf = input.decode().replace("\r", self.eol).encode() + self.respond(input)
+
+                self.buf += self.respond(input)
+
+                if input == b"gpio notify on\r" and self.ports != 8:
+                    self.notify = True
+                if input == b"gpio notify off\r" and self.ports != 8:
+                    self.notify = False
+
+
 
         def read(self, size):
             """Read size bytes from the mocked device buffer."""
