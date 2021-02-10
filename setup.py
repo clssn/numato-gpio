1602e2be0f8b15e8a0397c8312698af1918ba13f
Henning Claßen <code@clssn.de>
2021-02-10 22:37:02 +0100

    Bump version to v0.11.0.dev0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="numato-gpio",
-    version="0.10.0",
+    version="0.11.0.dev0",
     author="Henning Classen",
     author_email="code@clssn.de",
     description="Python API for Numato GPIO Expanders",
