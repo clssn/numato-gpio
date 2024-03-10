0ac573bb5c7394516b85e11954f34062e278db52
Henning Claßen <code@clssn.de>
2024-03-10 14:52:20 +0100

    Improve test performance regarding changed role of line endings

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,17 +3,7 @@ import serial
 import serialmock
 
 
-@pytest.fixture(params=["\r\n", "\n\r", "\r", "\n"], ids=["crlf", "lfcr", "cr", "lf"])
-def mock_device(monkeypatch, request):
-    """Mock the serial.Serial class with our serialmock.SerialMock.
-
-    Parametrized different response-line endings model the different line
-    endings different numato devices respond with, even with the same firmware
-    version.
-
-    Note:
-        cr = carriage return = "\r"
-        lf = line feed = "\n"
-
-    """
-    monkeypatch.setattr(serial, "Serial", serialmock.gen_serial(request.param))
+@pytest.fixture()
+def mock_device(monkeypatch):
+    """Mock the serial.Serial class with our serialmock.SerialMock."""
+    monkeypatch.setattr(serial, "Serial", serialmock.gen_serial())
