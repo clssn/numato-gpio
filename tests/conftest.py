dc535989c95bbaaec6405344f08dd5697d37aedf
Henning Claßen <code@clssn.de>
2024-03-10 18:26:05 +0100

    Raise when notification API used on non-supporting devices
    Also adapt and improve tests and fix pylint findings.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,4 @@
+"""Fixtures commonly used by tests."""
 import pytest
 import serial
 import serialmock
@@ -6,4 +7,4 @@ import serialmock
 @pytest.fixture()
 def mock_device(monkeypatch):
     """Mock the serial.Serial class with our serialmock.SerialMock."""
-    monkeypatch.setattr(serial, "Serial", serialmock.gen_serial())
+    monkeypatch.setattr(serial, "Serial", serialmock.SerialMock)
--- tests/test_notify.py
+++ tests/test_notify.py
@@ -1,9 +1,11 @@
+"""This module shall ensure that notifications are correctly handled anywhere in the input stream."""
+
 from unittest.mock import Mock
 
-import numato_gpio
 import pytest
-
 from common import PORTS, Position
+
+import numato_gpio
 
 
 @pytest.mark.parametrize("ports", PORTS)
@@ -15,29 +17,43 @@ from common import PORTS, Position
         Position.BACK,
     ],
 )
-def test_notify(ports, position, mock_device, monkeypatch):
+@pytest.mark.usefixtures("mock_device")
+def test_notify(ports, position, monkeypatch):
     """Test notifications."""
     monkeypatch.setattr("serial.Serial.ports", ports)
     dev = numato_gpio.NumatoUsbGpio("/dev/ttyACMxx")
-    dev.notify = True
-    msg = b"gpio readall\r"
-    msg_length = len(msg) - len("\r") + len(dev._ser.eol) * 2 + ports // 4
-    if position == Position.FRONT:
-        monkeypatch.setattr(dev._ser, "notify_inject_at", 0)
-    elif position == Position.CENTER:
-        monkeypatch.setattr(dev._ser, "notify_inject_at", msg_length // 2)
-    elif position == Position.BACK:
-        monkeypatch.setattr(dev._ser, "notify_inject_at", msg_length)
 
-    callbacks = []
-    if ports != 8:
-        for p in range(ports):
-            cb = Mock()
-            dev.setup(p, numato_gpio.IN)
+    # expect devices that don't support notifications to raise (but still proceed with tests)
+    if not dev.can_notify:
+        with pytest.raises(numato_gpio.NumatoGpioError):
+            dev.notify = True
+    else:
+        dev.notify = True
+
+    msg = b"gpio readall\r"
+    # Need the particular Serial (mock) instance created in the constructor of NumatoUsbGpio
+    serial_mock = dev._ser  # pylint: disable=protected-access
+    msg_length = len(msg) - len("\r") + len(serial_mock.eol) * 2 + ports // 4
+    if position == Position.FRONT:
+        monkeypatch.setattr(serial_mock, "notify_inject_at", 0)
+    elif position == Position.CENTER:
+        monkeypatch.setattr(serial_mock, "notify_inject_at", msg_length // 2)
+    elif position == Position.BACK:
+        monkeypatch.setattr(serial_mock, "notify_inject_at", msg_length)
+
+    port_callbacks = []
+    for p in range(ports):
+        cb = Mock()
+        dev.setup(p, numato_gpio.IN)
+        if dev.can_notify:
             dev.add_event_detect(p, cb, numato_gpio.BOTH)
-            callbacks.append(cb)
+        else:
+            with pytest.raises(numato_gpio.NumatoGpioError):
+                dev.add_event_detect(p, cb, numato_gpio.BOTH)
+        port_callbacks.append(cb)
     dev.readall()
-    if ports != 8:
-        for p in range(ports):
-            callbacks[p].assert_called_with(p, True)
+    if dev.can_notify:
+        assert all(cb.called_with(p, True) for cb in port_callbacks)
+    else:
+        assert not any(cb.called for cb in port_callbacks)
     dev.cleanup()
